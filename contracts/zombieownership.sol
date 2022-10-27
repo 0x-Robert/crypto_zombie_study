1b47af07f605ff30bf23be4083c2c26b7657f2f7
shypang <smartdragon417@gmail.com>
2022-10-27 15:07:20 +0900

    study erc721, safemath

--- contracts/zombieownership.sol
+++ contracts/zombieownership.sol
@@ -1,9 +1,46 @@
 pragma solidity ^0.4.19;
 
 import "./zombieattack.sol";
-// 여기서 import 하게.
+import "./erc721.sol";
+import "./safemath.sol";
 
-// 여기서 ERC721 상속을 선언하게.
-contract ZombieOwnership is ZombieAttack {
+/// TODO: natspec에 맞도록 이 부분을 바꾸게.
+//@title 좀비 소유권을 관리하는 컨트랙트
+//@author robert.seo
+//@dev openzeppelin의 ERC721 표준을 따름
+contract ZombieOwnership is ZombieAttack, ERC721 {
 
+  using SafeMath for uint256;
+
+  mapping (uint => address) zombieApprovals;
+
+  function balanceOf(address _owner) public view returns (uint256 _balance) {
+    return ownerZombieCount[_owner];
+  }
+
+  function ownerOf(uint256 _tokenId) public view returns (address _owner) {
+    return zombieToOwner[_tokenId];
+  }
+
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+    ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
+    ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].sub(1);
+    zombieToOwner[_tokenId] = _to;
+    Transfer(_from, _to, _tokenId);
+  }
+
+  function transfer(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+    _transfer(msg.sender, _to, _tokenId);
+  }
+
+  function approve(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+    zombieApprovals[_tokenId] = _to;
+    Approval(msg.sender, _to, _tokenId);
+  }
+
+  function takeOwnership(uint256 _tokenId) public {
+    require(zombieApprovals[_tokenId] == msg.sender);
+    address owner = ownerOf(_tokenId);
+    _transfer(owner, msg.sender, _tokenId);
+  }
 }
