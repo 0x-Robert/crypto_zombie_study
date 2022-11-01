17069747177b274090cae0769c4a247b75f3a9d7
shypang <smartdragon417@gmail.com>
2022-11-01 18:56:24 +0900

    done chainlink

--- Chainlink_Data/Contract.sol
+++ Chainlink_Data/Contract.sol
@@ -30,13 +30,15 @@ contract ZombieFactory is VRFConsumerbase {
         zombies.push(Zombie(_name, _dna));
     }
 
-    // 1. Create the `getRandomNumber` function
 
-    // 2. Create the `fulfillRandomness` function
-
-    function _generatePseudoRandomDna(string memory _str) private view returns (uint) {
-        uint rand = uint(keccak256(abi.encodePacked(_str)));
-        return rand % dnaModulus;
+    function getRandomNumber() public returns (bytes32 requestId) {
+        return requestRandomness(keyHash, fee);
     }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult = randomness;
+    }
+
+
 
 }
