6fe2e7c6333990194d780b3fa536f63ed863b029
shypang <smartdragon417@gmail.com>
2022-11-03 19:07:04 +0900

    testing smart contract with truffle

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -1,5 +1,8 @@
 const CryptoZombies = artifacts.require("CryptoZombies");
 const utils = require("./helpers/utils");
+const time = require("./helpers/time");
+//TODO: import expect into our project
+var expect = require("chai").expect;
 const zombieNames = ["Zombie 1", "Zombie 2"];
 contract("CryptoZombies", (accounts) => {
   let [alice, bob] = accounts;
@@ -11,8 +14,9 @@ contract("CryptoZombies", (accounts) => {
     const result = await contractInstance.createRandomZombie(zombieNames[0], {
       from: alice,
     });
-    assert.equal(result.receipt.status, true);
-    assert.equal(result.logs[0].args.name, zombieNames[0]);
+    //TODO: replace with expect
+    expect(result.receipt.status).to.equal(true);
+    expect(result.logs[0].args.name).to.equal(zombieNames[0]);
   });
   it("should not allow two zombies", async () => {
     await contractInstance.createRandomZombie(zombieNames[0], { from: alice });
@@ -30,7 +34,9 @@ contract("CryptoZombies", (accounts) => {
         from: alice,
       });
       const newOwner = await contractInstance.ownerOf(zombieId);
-      assert.equal(newOwner, bob);
+      //TODO: replace with expect
+      //assert.equal(newOwner, bob);
+      expect(newOwner).to.equal(bob);
     });
   });
   context("with the two-step transfer scenario", async () => {
@@ -42,10 +48,11 @@ contract("CryptoZombies", (accounts) => {
       await contractInstance.approve(bob, zombieId, { from: alice });
       await contractInstance.transferFrom(alice, bob, zombieId, { from: bob });
       const newOwner = await contractInstance.ownerOf(zombieId);
-      assert.equal(newOwner, bob);
+      //TODO: replace with expect
+      //assert.equal(newOwner,bob);
+      expect(newOwner).to.equal(bob);
     });
     it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
-      // TODO: start
       const result = await contractInstance.createRandomZombie(zombieNames[0], {
         from: alice,
       });
@@ -55,7 +62,27 @@ contract("CryptoZombies", (accounts) => {
         from: alice,
       });
       const newOwner = await contractInstance.ownerOf(zombieId);
-      assert.equal(newOwner, bob);
+      //TODO: replace with expect
+      //assert.equal(newOwner,bob);
+      expect(newOwner).to.equal(bob);
     });
+  });
+  it("zombies should be able to attack another zombie", async () => {
+    let result;
+    result = await contractInstance.createRandomZombie(zombieNames[0], {
+      from: alice,
+    });
+    const firstZombieId = result.logs[0].args.zombieId.toNumber();
+    result = await contractInstance.createRandomZombie(zombieNames[1], {
+      from: bob,
+    });
+    const secondZombieId = result.logs[0].args.zombieId.toNumber();
+    await time.increase(time.duration.days(1));
+    await contractInstance.attack(firstZombieId, secondZombieId, {
+      from: alice,
+    });
+    //TODO: replace with expect
+    //assert.equal(result.receipt.status, true);
+    expect(result.receipt.status).to.equal(true);
   });
 });
