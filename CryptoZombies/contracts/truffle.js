8432ef864d526ff99c08813317e309df8b7d3892
shypang <smartdragon417@gmail.com>
2022-11-06 23:34:24 +0900

    loom network test and truffle test

---
+++ CryptoZombies/contracts/truffle.js
@@ -0,0 +1,50 @@
+// Initialize HDWalletProvider
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+// 1. Initialize LoomTruffleProvider
+const LoomTruffleProvider = require("loom-truffle-provider");
+// Set your own mnemonic here
+const mnemonic = "YOUR_MNEMONIC";
+
+// Module exports to make this configuration available to Truffle itself
+module.exports = {
+  // Object with configuration for each network
+  networks: {
+    // Configuration for mainnet
+    mainnet: {
+      provider: function () {
+        // Setting the provider with the Infura Rinkeby address and Token
+        return new HDWalletProvider(
+          mnemonic,
+          "https://mainnet.infura.io/v3/YOUR_TOKEN"
+        );
+      },
+      network_id: "1",
+    },
+    // Configuration for rinkeby network
+    rinkeby: {
+      // Special function to setup the provider
+      provider: function () {
+        // Setting the provider with the Infura Rinkeby address and Token
+        return new HDWalletProvider(
+          mnemonic,
+          "https://rinkeby.infura.io/v3/YOUR_TOKEN"
+        );
+      },
+      // Network id is 4 for Rinkeby
+      network_id: 4,
+    },
+
+    // 2. Put here the configuration for loom_dapp_chain
+    loom_testnet: {
+      provider: function () {
+        const privateKey = "YOUR_PRIVATE_KEY";
+        const chainId = "extdev-plasma-us1";
+        const writeUrl = "http://extdev-plasma-us1.dappchains.com:80/rpc";
+        const readUrl = "http://extdev-plasma-us1.dappchains.com:80/query";
+        return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey);
+      },
+      network_id: "9545242630824",
+    },
+  },
+};
